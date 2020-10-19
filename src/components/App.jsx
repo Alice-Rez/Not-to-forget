0bf81c7cde60b2f990da0b646eb0853b9903d1a4
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-19 21:11:25 +0200

    small corections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="wrapper">
         <Header />
         <main className="p-3">
           <Navigation
