8b08b17d052c25ecda02f929ada6bceb89535282
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-19 21:29:48 +0200

    change pt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Header extends Component {
   render() {
     return (
-      <header className="text-center pt-5">
+      <header className="text-center pt-3">
         <h1 className="display-2">Not To Forget</h1>
       </header>
     );
