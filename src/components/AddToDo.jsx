c6cd8e5ad72a53bc1c43de9620c0496c0cad3bab
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-20 10:18:09 +0200

    added importance and details inputs

--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 
 export default class AddToDo extends Component {
   state = {
-    title: "",
-    deadline: "",
+    task: {},
   };
 
   getValue = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({
+      task: { ...this.state.task, [e.target.name]: e.target.value },
+    });
   };
   render() {
     return (
@@ -15,7 +16,7 @@ export default class AddToDo extends Component {
         action=""
         className="container text-center border py-5 d-flex flex-column align-items-center col-sm-5 bg-white my-5"
       >
-        <h2>Add new item</h2>
+        <h2 className="pb-3">Add new item</h2>
         <div className="form-group col-sm-10 row">
           <label htmlFor="title" className="col-sm-2 col-form-label ">
             Title
@@ -40,15 +41,45 @@ export default class AddToDo extends Component {
             onInput={this.getValue}
           />
         </div>
+        <div className="form-group col-sm-10 row">
+          <label htmlFor="importance" className="col-sm-2 col-form-label ">
+            Level
+          </label>
+          <select
+            name="importance"
+            id="importance"
+            className="form-control col-sm-10"
+            onChange={this.getValue}
+          >
+            <option value="">----- Please choose one option -----</option>
+            <option value="Yesterday was late!">Yesterday was late!</option>
+            <option value="It is time to look at it">
+              It is time to look at it
+            </option>
+            <option value="What you can do today, you can also do tomorrow">
+              What you can do today, you can also do tomorrow
+            </option>
+          </select>
+        </div>
+        <div className="form-group col-sm-10 row">
+          <label htmlFor="description" className="col-sm-2 col-form-label ">
+            Details
+          </label>
+          <textarea
+            cols="30"
+            rows="3"
+            name="description"
+            id="description"
+            className="form-control col-sm-10"
+            onInput={this.getValue}
+          />
+        </div>
         <button
           type="submit"
           className="btn btn-custom col-sm-4"
           onClick={(e) => {
             e.preventDefault();
-            this.props.addTask({
-              title: this.state.title,
-              deadline: this.state.deadline,
-            });
+            this.props.addTask(this.state.task);
           }}
         >
           Add
