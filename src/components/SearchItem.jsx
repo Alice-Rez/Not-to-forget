f179976f4a9fa876cb3afa4ebfd3840cdfb9eb1c
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-22 23:55:52 +0200

    implmented hover ons earch item

--- src/components/SearchItem.jsx
+++ src/components/SearchItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function SearchItem(props) {
-  return <li class="list-group-item">{props.title}</li>;
+  return <li class="list-group-item text-left">{props.title}</li>;
 }
