5eec2305ac6cc76403d29493798ef6b9df95aceb
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-28 20:17:23 +0100

    changed display of date

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,7 +4,9 @@ export default function Item(props) {
   return (
     <article className="border card-white">
       <h3>{props.task.title}</h3>
-      <p className="test">{props.task.deadline}</p>
+      <p className="test">
+        {props.task.deadline.split("-").reverse().join(".")}
+      </p>
       <p>{props.task.importance}</p>
       <p>{props.task.description}</p>
     </article>
