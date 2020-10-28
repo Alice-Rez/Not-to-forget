40f2638985ccea675a2e4636858ca4f3ae00060a
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-28 16:35:48 +0100

    Navigation style in separate file

--- src/index.jsx
+++ src/index.jsx
@@ -4,5 +4,6 @@ import App from "./components/App";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import "./styles/Header.css";
+import "./styles/Navigation.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
