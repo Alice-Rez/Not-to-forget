364101057d6ede594fbad3c52487f471795bb6e1
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-28 20:01:47 +0100

    added more style and more info to items

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function Item(props) {
   return (
     <article className="border card-white">
-      <h4>{props.task.title}</h4>
+      <h3>{props.task.title}</h3>
+      <p>{props.task.deadline}</p>
+      <p>{props.task.importance}</p>
+      <p>{props.task.description}</p>
     </article>
   );
 }
