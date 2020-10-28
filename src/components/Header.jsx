dac5013877c9244df85ebb0f5795e9f73b4e3572
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-28 16:09:33 +0100

    created file just for header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
+// import "../styles/Header.css";
 import Navigation from "./Navigation";
 
 export default class Header extends Component {
   render() {
     return (
       <header className="pt-3">
-        <h1>Not To Forget</h1>
+        <div>
+          <h1>Not To Forget</h1>
+          {/* <p className="lead">To-do application for your help</p> */}
+        </div>
         <Navigation
           showAdd={this.props.showAdd}
           formVisible={this.props.formVisible}
--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "./index.css";
+import "./styles/Header.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
