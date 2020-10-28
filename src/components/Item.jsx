9f58baaae6e762793621666dd2c2687596ab8dc4
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-28 20:23:07 +0100

    levels description added

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 
 export default function Item(props) {
+  const levels = [
+    "Yesterday was late",
+    "It is time to look at it ;)",
+    "No rush......",
+    "Too early to just think about it",
+  ];
+
   return (
     <article className="border card-white">
       <h3>{props.task.title}</h3>
       <p className="test">
         {props.task.deadline.split("-").reverse().join(".")}
       </p>
-      <p>{props.task.importance}</p>
+      <p>{levels[props.task.importance - 1]}</p>
       <p>{props.task.description}</p>
     </article>
   );
