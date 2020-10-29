4c3e25701f572450f815f7d9cba43ee23bf1ab38
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-29 22:56:40 +0100

    adde buttons for later sorting

--- src/components/ListToDo.jsx
+++ src/components/ListToDo.jsx
@@ -4,13 +4,18 @@ import Item from "./Item";
 export default class ListToDo extends Component {
   render() {
     return (
-      <section>
+      <React.Fragment>
+        <section className="buttons-group">
+          <button className="btn btn-lg btn-list">&#11015; Importance</button>
+          <button className="btn btn-lg btn-list">&#11015; Deadline</button>
+          <button className="btn btn-lg btn-list">&#8681; Title</button>
+        </section>
         <section className="list-card-deck">
           {this.props.tasks.map((task, index) => (
             <Item key={`task-${index + 1}`} task={task} />
           ))}
         </section>
-      </section>
+      </React.Fragment>
     );
   }
 }
