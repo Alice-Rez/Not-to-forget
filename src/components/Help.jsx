2f5e9b16fcad6654a864429bbdb3f51604a55cc5
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-29 23:24:55 +0100

    added some text to help

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -3,8 +3,20 @@ import React, { Component } from "react";
 export default class Help extends Component {
   render() {
     return (
-      <section className="section-main card-white">
+      <section className="section-main card-white help">
         <h2>Clever management of your to-dos</h2>
+        <p>
+          Never more need to worry about forgetting important things - work
+          deadlines, taxes deadlines or birthdays of your significant others
+          <span role="img" aria-labelledby="grinning face">
+            😁
+          </span>
+          .
+        </p>
+        <p>
+          Explore advantages of Not to Forget in demo version and than start
+          your own to-do management by signing-up.
+        </p>
       </section>
     );
   }
