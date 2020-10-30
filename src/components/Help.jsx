4b8b9b58b874311d44b40e2da80465f24f92a707
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-30 16:27:29 +0100

    added and styled content

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Help extends Component {
   render() {
     return (
-      <section>
+      <React.Fragment>
         <section className="section-main">
           <h2>Clever management of your to-dos</h2>
           <p className="lead-p">
@@ -17,17 +17,131 @@ export default class Help extends Component {
         </section>
         <section className="my-card-deck">
           <article className="card-white">
+            <figure>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                id="SVGRoot"
+                viewBox="0 0 16 16"
+                role="presentation"
+                focusable="false"
+              >
+                <g id="circle" fill-opacity="1">
+                  <circle
+                    id="path1453"
+                    cx="7.9812"
+                    cy="8.0188"
+                    r="7.9812"
+                    fill="#407076"
+                  />
+                  <path
+                    d="M9.3979 9.4605h2.522V6.6233h-2.522v-2.522H6.5606v2.522h-2.522v2.8372h2.522v2.522H9.398v-2.522z"
+                    id="path1436-3"
+                    fill="#fff"
+                  />
+                </g>
+              </svg>
+            </figure>
             <p>
               <strong>Add to-do items</strong>
             </p>
-            <p></p>
+            <p>
+              You have to add title, deadline and importance. Detailed
+              description is optional
+            </p>
           </article>
           <article className="card-white">
-            <p>explore advantages of Not to Forget in demo version</p>
-            <p>or</p>
-            <p>start your own to-do management by signing-up.</p>
+            <figure>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                id="SVGRoot"
+                viewBox="0 0 16 16"
+                role="presentation"
+                focusable="false"
+              >
+                <g id="layer1" fill-opacity="1">
+                  <circle
+                    id="path1453"
+                    cx="7.981"
+                    cy="8.019"
+                    r="7.981"
+                    fill="#e1ca96"
+                  />
+                  <path
+                    d="M8.2622 10.4074c.2004-.1325.2905-.3042.3985-.7589.1626-.6848.2397-.7855 1.435-1.872.971-.8826 1.2424-1.2986 1.3224-2.0277.1598-1.4572-.9328-2.7182-2.5963-2.9964-.543-.0908-1.4185-.0632-1.8684.059-.8922.2422-1.6897.8213-2.1134 1.5344-.3422.576-.4635 1.261-.2843 1.6057.2158.4152.7.6236 1.116.4801.3096-.1067.4633-.3013.6481-.8207.2405-.6756.5414-1.1049.896-1.278.2765-.1351.7786-.202 1.0965-.146.7226.1273 1.192.6328 1.1946 1.2861.0019.4808-.1958.7814-.9492 1.4432-1.1726 1.0302-1.4692 1.4485-1.5932 2.2474-.1373.884.1893 1.3926.8749 1.3626.2055-.009.2947-.034.4228-.1188z"
+                    id="path3487-9"
+                    fill="#fff"
+                  />
+                  <ellipse
+                    id="path3534"
+                    cx="7.8437"
+                    cy="12.2638"
+                    rx="1.0625"
+                    ry="1.0884"
+                    fill="#fff"
+                  />
+                </g>
+              </svg>
+            </figure>
+            <p>
+              <strong>Start with app</strong>
+            </p>
+            <p>Explore advantages of Not to Forget in demo version</p>
+            <p>OR</p>
+            <p>Start your own to-do management by signing-up.</p>
           </article>
           <article className="card-white">
+            <figure>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                id="SVGRoot"
+                viewBox="0 0 16 16"
+                role="presentation"
+                focusable="false"
+              >
+                <g id="layer1" fill-opacity="1">
+                  <circle
+                    id="path1453"
+                    cx="7.981"
+                    cy="8.019"
+                    r="7.981"
+                    fill="#f28482"
+                  />
+                  <path
+                    d="M12.7338 10.8357c.1114-.0596.1592-.255.1592-.651 0-.4228-.045-.5827-.1812-.6447-.0712-.0325-1.103-.0449-3.744-.0449-3.6666 0-3.7595.0033-3.8468.134-.016.0238-.0292.2727-.0296.553-.0005.4696.0062.517.0851.601l.0857.0912H8.962c2.338 0 3.7263-.0142 3.7718-.0386z"
+                    id="path2092-6"
+                    fill="#fff"
+                  />
+                  <path
+                    d="M4.3452 8.5918c.1147-.1146.1152-.1173.1152-.594 0-.4768-.0005-.4795-.1152-.5942-.1145-.1145-.1179-.1151-.5889-.1151-.444 0-.4806.0059-.5842.095l-.1106.0952v1.0381l.1106.0951c.1036.0892.1403.0951.5842.0951.471 0 .4744-.0006.5889-.1152z"
+                    id="path2092-6-7"
+                    fill="#fff"
+                  />
+                  <path
+                    d="M4.3787 10.7705c.1147-.1147.1152-.1173.1152-.5941 0-.4768-.0005-.4795-.1152-.5941-.1145-.1146-.1179-.1152-.5889-.1152-.4439 0-.4806.0059-.5842.095l-.1106.0952v1.0381l.1106.0951c.1036.0892.1403.0951.5842.0951.471 0 .4744-.0006.5889-.1151z"
+                    id="path2092-6-7-5"
+                    fill="#fff"
+                  />
+                  <path
+                    d="M4.3637 6.4036c.1146-.1146.1151-.1173.1151-.594 0-.4768-.0005-.4795-.1151-.5942-.1146-.1145-.118-.1151-.589-.1151-.4439 0-.4806.0059-.5842.095l-.1105.0952v1.0381l.1105.0951c.1036.0892.1403.0951.5843.0951.471 0 .4743-.0006.5889-.1152z"
+                    id="path2092-6-7-3"
+                    fill="#fff"
+                  />
+                  <path
+                    d="M12.7835 8.5976c.1114-.0596.1592-.255.1592-.651 0-.4229-.0449-.5827-.1811-.6448-.0713-.0325-1.1032-.0448-3.7441-.0448-3.6666 0-3.7595.0032-3.8468.134-.0159.0238-.0292.2727-.0295.553-.0006.4696.0061.517.085.601l.0857.0911h3.6998c2.338 0 3.7263-.0142 3.7718-.0385z"
+                    id="path2092-6-5"
+                    fill="#fff"
+                  />
+                  <path
+                    d="M12.7338 6.4618c.1114-.0596.1592-.255.1592-.651 0-.4229-.045-.5827-.1812-.6448-.0712-.0324-1.103-.0448-3.744-.0448-3.6666 0-3.7595.0032-3.8468.134-.016.0238-.0292.2727-.0296.553-.0005.4696.0062.517.0851.601l.0857.0912H8.962c2.338 0 3.7263-.0142 3.7718-.0386z"
+                    id="path2092-6-6"
+                    fill="#fff"
+                  />
+                </g>
+              </svg>
+            </figure>
             <p>
               <strong>List to-do items</strong>
             </p>
@@ -36,8 +150,47 @@ export default class Help extends Component {
               title or reset to the default order of adding date
             </p>
           </article>
+          <article className="card-white">
+            <figure>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                id="SVGRoot"
+                viewBox="0 0 16 16"
+                role="presentation"
+                focusable="false"
+              >
+                <g id="layer1" fill-opacity="1">
+                  <circle
+                    id="path1453"
+                    cx="7.981"
+                    cy="8.019"
+                    r="7.981"
+                    fill="#bba0ca"
+                  />
+                  <path
+                    d="M12.9317 12.613c.113-.043.2584-.2544.2586-.376 0-.0437-.0309-.1346-.0688-.202-.038-.0674-.6985-.721-1.4679-1.4525a1486.7228 1486.7228 0 01-1.5085-1.4354l-.1096-.1053.1776-.2706c.2199-.3346.3899-.7373.5029-1.191.126-.5057.1177-1.3016-.019-1.8251-.1764-.6751-.4805-1.2251-.956-1.7292-.3619-.3836-.6742-.6119-1.1412-.834-1.694-.8058-3.6531-.3281-4.7935 1.1687-1.587 2.0831-.6556 5.1206 1.8456 6.0184 1.166.4185 2.628.1856 3.6079-.575.0812-.063.1645-.1087.1849-.1014.0205.0072.7056.6515 1.5226 1.4317s1.5228 1.4401 1.5686 1.4664c.1018.0586.261.0635.3958.0122zm-6.381-2.922c-.9781-.1246-1.827-.7098-2.2824-1.5735-.2569-.4873-.3471-.8555-.3471-1.4162 0-1.0468.4704-1.9322 1.333-2.5086.5195-.3472 1.025-.4952 1.692-.4955C8.0772 3.697 9.075 4.3064 9.6 5.3186c.278.5358.3826 1.0975.3162 1.6979-.0902.8158-.463 1.5083-1.0785 2.0035-.6783.5456-1.4633.776-2.2869.671z"
+                    id="path2818-2"
+                    fill="#fff"
+                  />
+                </g>
+              </svg>
+            </figure>
+            <p>
+              <strong>Search to-do items</strong>
+            </p>
+            <p>
+              You want quickly find just one specific to-do? Search for its
+              name, autocomplete function will show you all your to-dos starting
+              with whatever you typed in input.
+            </p>
+            <p>
+              After clicking on search button, the to-do item will be displayed
+              and available for editing.
+            </p>
+          </article>
         </section>
-      </section>
+      </React.Fragment>
     );
   }
 }
