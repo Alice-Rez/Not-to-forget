d6eeee9fff530b15ac98fec2dae0038faf1ad29f
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-30 16:06:23 +0100

    adding more info in Help

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -3,20 +3,40 @@ import React, { Component } from "react";
 export default class Help extends Component {
   render() {
     return (
-      <section className="section-main card-white help">
-        <h2>Clever management of your to-dos</h2>
-        <p>
-          Never more need to worry about forgetting important things - work
-          deadlines, taxes deadlines or birthdays of your significant others
-          <span role="img" aria-labelledby="grinning face">
-            😁
-          </span>
-          .
-        </p>
-        <p>
-          Explore advantages of Not to Forget in demo version and than start
-          your own to-do management by signing-up.
-        </p>
+      <section>
+        <section className="section-main">
+          <h2>Clever management of your to-dos</h2>
+          <p className="lead-p">
+            Never more need to worry about forgetting important things - work
+            deadlines, taxes deadlines or birthdays of your significant others{" "}
+            <span role="img" aria-labelledby="grinning face">
+              😁
+            </span>
+            .
+          </p>
+        </section>
+        <section className="my-card-deck">
+          <article className="card-white">
+            <p>
+              <strong>Add to-do items</strong>
+            </p>
+            <p></p>
+          </article>
+          <article className="card-white">
+            <p>explore advantages of Not to Forget in demo version</p>
+            <p>or</p>
+            <p>start your own to-do management by signing-up.</p>
+          </article>
+          <article className="card-white">
+            <p>
+              <strong>List to-do items</strong>
+            </p>
+            <p>
+              And sort them by importance level, deadline, alphabetically by
+              title or reset to the default order of adding date
+            </p>
+          </article>
+        </section>
       </section>
     );
   }
