cdf77c69a88d6f6fb324875ea3aaeee582ce992e
Alice-Rez <reznickova.alice@seznam.cz>
2021-01-12 22:31:42 +0100

    changed index on order

--- backend/model/TaskModel.js
+++ backend/model/TaskModel.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 
 let TaskSchema = new mongoose.Schema({
-  index: { type: Number, unique: true, required: true },
+  order: { type: Number, required: true },
   title: { type: String, required: true },
   deadline: { type: String, required: true },
   importance: { type: String, required: true, enum: ["1", "2", "3", "4"] },
